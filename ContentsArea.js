6d4eb8f7333fe38e907253c164ac1d9dd6e37f74
YeonLog <jyb0422@naver.com>
2023-12-03 03:43:18 +0900

    Feat:Contents 영역 "추가 설명", "#나눔 희망 일시", "#나눔 희망 장소"

--- ContentsArea.js
+++ ContentsArea.js
@@ -1,15 +1,58 @@
-import React from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 const ContentsArea = () => {
+  const [description, setDescription] = useState('');
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleDateConfirm = (date) => {
+    setSelectedDate(date);
+    hideDatePicker();
+  };
+
+  const handleLocationButtonClick = () => {
+    // RegisterNewItemMap.js로 이동하는 코드 추가
+    // 예를 들어, navigation.navigate('RegisterNewItemMap');
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Contents</Text>
+      <Text style={styles.title}>추가 설명</Text>
       <TextInput
-        style={styles.input}
-        placeholder="Enter contents here"
+        style={styles.textarea}
+        placeholder="물건에 대한 설명을 입력해주세요"
         multiline
+        value={description}
+        onChangeText={(text) => setDescription(text)}
+        maxLength={65535}
+        placeholderTextColor="#B8B8B8"
       />
+
+      <Text style={styles.title}>#나눔 희망 일시</Text>
+      <TouchableOpacity onPress={showDatePicker} style={styles.dateButton}>
+        <Text style={styles.buttonText}>일시 추가</Text>
+      </TouchableOpacity>
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="datetime"
+        onConfirm={handleDateConfirm}
+        onCancel={hideDatePicker}
+      />
+
+      <Text style={styles.title}>#나눔 희망 장소</Text>
+      <TouchableOpacity onPress={handleLocationButtonClick} style={styles.locationButton}>
+        <Text style={styles.buttonText}>장소 추가</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -22,13 +65,34 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginTop: 10,
   },
-  input: {
+  textarea: {
     height: 100,
     borderColor: 'gray',
     borderWidth: 1,
     marginTop: 10,
     padding: 8,
+    color: '#B8B8B8',
+  },
+  dateButton: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  locationButton: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
--- TitleArea.js
+++ TitleArea.js
@@ -1,30 +1,24 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, TextInput, Image } from 'react-native';
-import ImagePicker from 'react-native-image-picker'; // 이미지 피커 라이브러리 추가
+import ImagePicker from 'react-native-image-picker';
 
 const TitleArea = () => {
-  // 상태 변수 정의
-  const [title, setTitle] = useState(''); // 제목을 담는 상태 변수
-  const [selectedImages, setSelectedImages] = useState([]); // 선택된 이미지를 담는 상태 변수
+  const [title, setTitle] = useState('');
+  const [selectedImages, setSelectedImages] = useState([]);
 
-  // 카메라로 사진 찍기 처리 함수
   const handleCameraCapture = () => {
-
     const options = {
-      title: 'Select Photo', 
+      title: 'Select Photo',
       storageOptions: {
         skipBackup: true,
         path: 'images',
       },
     };
 
-    // 이미지 피커 라이브러리 사용하여 사진 찍기
     ImagePicker.showImagePicker(options, (response) => {
       if (response.didCancel || response.error) {
-        // 사용자가 취소하거나 오류 발생 시 처리
         console.log('Camera launch cancelled or encountered an error');
       } else {
-        // 선택된 이미지를 추가
         setSelectedImages((prevImages) => [...prevImages, { uri: response.uri }]);
       }
     });
@@ -32,22 +26,18 @@ const TitleArea = () => {
 
   return (
     <View style={styles.container}>
-      {/* 카메라 버튼 */}
       <TouchableOpacity onPress={handleCameraCapture} style={styles.cameraButton}>
-        <Text style={styles.cameraButtonText}>사진 추가</Text>
+        <Text style={styles.cameraButtonText}>Take Photo</Text>
       </TouchableOpacity>
 
-      {/* 선택된 이미지 표시 영역 */}
       {selectedImages.length > 0 && (
         <View style={styles.imageContainer}>
-          {/* 선택된 이미지 표시 */}
           {selectedImages.map((image, index) => (
             <Image key={index} source={{ uri: image.uri }} style={styles.image} />
           ))}
         </View>
       )}
 
-     {/* 제목 */} 
       <Text style={styles.titleText}>제목</Text>
       <TextInput
         style={styles.input}
@@ -61,7 +51,6 @@ const TitleArea = () => {
   );
 };
 
-// 스타일 정의
 const styles = StyleSheet.create({
   container: {
     padding: 10,
